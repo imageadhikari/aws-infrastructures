b6054725cb5ad04da3ff00cfa29df2d88d0036ef
imageadhikari <imageadhikari@gmail.com>
2023-03-28 16:54:07 +0545

    Updated rds

--- main.tf
+++ main.tf
@@ -13,29 +13,32 @@ provider "aws" {
   profile = "default"
 }
 
-module "vpc" {
-  source                    = "./modules/vpc"
-  region                    = 
-  vpc-cidr                  = 
-  public-subnet-1-cidr      = 
-  public-subnet-2-cidr      = 
-  private-subnet-1-cidr     =
-  private-subnet-2-cidr     =  
-  private-subnet-3-cidr     =
-  private-subnet-4-cidr     =  
-}
+# module "vpc" {
+#   source                    = "./modules/vpc"
+#   region                    = 
+#   vpc-cidr                  = 
+#   public-subnet-1-cidr      = 
+#   public-subnet-2-cidr      = 
+#   private-subnet-1-cidr     =
+#   private-subnet-2-cidr     =  
+#   private-subnet-3-cidr     =
+#   private-subnet-4-cidr     =  
+# }
 
-module "ec2" {
-    source                    = "./modules/ec2"
-    ami_id                    = 
-    instance_type             =
-    vpc_id                    = 
-    name                      = 
-    tags                      = 
-    subnet_id                 = 
-}
+# module "ec2" {
+#     source                    = "./modules/ec2"
+#     ami_id                    = 
+#     instance_type             =
+#     vpc_id                    = 
+#     name                      = 
+#     tags                      = 
+#     subnet_id                 = 
+# }
 
 module "rds" {
-  source = "./modules/rds"
-  
+  source                        = "./modules/rds"
+  # instance_class                = 
+  # storage                       =
+  # engine                        = 
+  # engine-version                =  
 }
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -12,9 +12,15 @@ resource "aws_db_instance" "default" {
 resource "aws_security_group" "example" {
   name_prefix = "rds-db"
   ingress {
-    from_port   = 0
+    from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
