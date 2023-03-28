8c2c64b202e81a855c55c35d138768b5770ac9a6
imageadhikari <imageadhikari@gmail.com>
2023-03-28 15:17:50 +0545

    ec2 and vpc updated

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source                    = "./modules/vpc"
+  region                    = var.region
+  vpc-cidr                  = 
+  public-subnet-1-cidr      =
+  public-subnet-2-cidr      = 
+  private-subnet-1-cidr     =
+  private-subnet-2-cidr     =  
+  private-subnet-3-cidr     =
+  private-subnet-4-cidr     =  
+}
+
+module "ec2" {
+    source                    = "./modules/ec2"
+    ami_id                    = 
+    instance_type             =
+    vpc_id                    = 
+    name                      = 
+    tags                      = 
+    subnet_id                 = 
+}
+
+module "rds" {
+  source = "./modules/rds"
+  
 }
