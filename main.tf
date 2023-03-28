1873191b73820a02a4e8c9537b771987da62c8a8
imageadhikari <imageadhikari@gmail.com>
2023-03-28 14:18:43 +0545

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
 module "vpc" {
   source = "./modules/vpc"
-
-  # The rest of arguments are omitted for brevity
-
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
 }
