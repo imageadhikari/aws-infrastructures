7ea9e81c56a25442170157c41145a3224dc182df
imageadhikari <imageadhikari@gmail.com>
2023-04-03 12:49:12 +0545

    target group attchment complete

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -67,15 +67,18 @@ resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
-
+  # default_action {
+  #   type = "fixed-response"
+  #   target_group_arn = aws_lb_target_group.alb_target_group.arn
+  #   fixed_response {
+  #     content_type = "text/plain"
+  #     message_body = "Fixed response content"
+  #     status_code  = "200"
+  #   }
+  # }
   default_action {
-    type = "fixed-response"
+    type = "forward"
     target_group_arn = aws_lb_target_group.alb_target_group.arn
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
   }
 }
 
